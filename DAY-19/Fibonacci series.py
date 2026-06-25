c7986e3aaba12ba09cffb51f8b6d3cce89ff689c
Devapriyadarshini <kalahastridevapriyadrashni9@gmail.com>
2026-06-25 11:34:53 +0530

    Update Fibonacci series.py

--- DAY-19/Fibonacci series.py
+++ DAY-19/Fibonacci series.py
@@ -6,3 +6,14 @@ def fib(n,memo={}):
     memo[n]= fib(n-1)+fib(n-2)
     return memo[n]
 print(fib(9))
+
+# Fibonacci Series With Tabulation Method
+def fib(n):
+    if n <= 1:
+        return n
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    for i in range(2, n + 1):
+        dp[i] = dp[i-1] + dp[i-2]
+    return dp[n]
+print(fib(5))
