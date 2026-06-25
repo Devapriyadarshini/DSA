0d2b89413823d44c74fc9705a98164627bcdf3ec
Devapriyadarshini <kalahastridevapriyadrashni9@gmail.com>
2026-06-25 11:50:16 +0530

    Update Fibonacci series.py

--- DAY-19/Fibonacci series.py
+++ DAY-19/Fibonacci series.py
@@ -17,3 +17,22 @@ def fib(n):
         dp[i] = dp[i-1] + dp[i-2]
     return dp[n]
 print(fib(5))
+
+# The space complexity of this code is o(1)
+
+def fib(n):
+    if n <= 1:
+        return n
+    
+    # We only store the two previous numbers, not the whole sequence
+    prev2 = 0  # Represents dp[i-2]
+    prev1 = 1  # Represents dp[i-1]
+    
+    for i in range(2, n + 1):
+        current = prev1 + prev2
+        prev2 = prev1   # Move the pointer forward
+        prev1 = current # Move the pointer forward
+        
+    return prev1
+
+print(fib(9))
